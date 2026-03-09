468adeccc0bfdb455deae07840733881cd6da49a
YEONJI-P <yeonji.park.kr@gmail.com>
2026-03-10 00:37:42 +0900

    first commit

---
+++ src/test/java/dev/yeon/iotsensorplatform/IotSensorPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.yeon.iotsensorplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotSensorPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
