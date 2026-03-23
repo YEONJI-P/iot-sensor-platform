4c4f1cf30a8095de7cd0140b2c1a39dc56874c85
YEONJI-P <yeonji.park.kr@gmail.com>
2026-03-24 03:18:04 +0900

    feat: Swagger CORS 관련

--- src/main/java/dev/yeon/iotsensorplatform/global/config/SwaggerConfig.java
+++ src/main/java/dev/yeon/iotsensorplatform/global/config/SwaggerConfig.java
@@ -5,14 +5,18 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .servers(List.of(new Server().url("/")))
                 .info(new Info()
                         .title("IoT Sensor Platformq API")
                         .description("IoT 센서 모니터링 플랫폼 API 문서")
